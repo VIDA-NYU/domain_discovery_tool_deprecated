1b4e69f5f129172b5b5890154af43c933f56c72b
yamuna <yamunak@gmail.com>
2015-02-19 13:46:23 -0500

    Created a rank class

--- ranking/rank.py
+++ ranking/rank.py
@@ -4,6 +4,34 @@ import tfidf
 import BayesianSets
 import numpy as np
 
+class rank:
+    def results(self,input_file,query_index):
+        table = tfidf.tfidf()
+    
+        # Compute tfidf of terms in the documents
+        table.process(input_file)
+        
+        data = table.getTfidfArray()
+        
+        print np.shape(data)
+
+        bs = BayesianSets.BayesianSets()
+    
+        # documents other than the relevant documents
+        index = [x for x in range(0,len(data)) if x not in query_index]
+
+        score = bs.score(data[query_index,:], data[index,:])
+
+        rank_index = np.argsort(np.multiply(score,-1))
+
+        offset_rank_index = [index[x] for x in rank_index]
+
+        # Get the urls corresponding to the scored documents
+        ranked_urls = table.getURLs(offset_rank_index)
+
+        ranked_scores = [score[rank_index[i]] for i in range(0, len(score))]
+        return [ranked_urls,ranked_scores]
+
 def main(argv):
     if len(argv) != 2:
         print "Invalid arguments"
@@ -14,30 +42,11 @@ def main(argv):
     input_file = argv[0]
     # Most relevant documents
     query_index = [int(i) for i in argv[1].split(',')]
+    ranker = rank()
+    [ranked_urls,scores] = ranker.results(input_file,query_index)
 
-    table = tfidf.tfidf()
+    for i in range(0,len(ranked_urls)):
+        print ranked_urls[i]," ", str(scores[i])
     
-    # Compute tfidf of terms in the documents
-    table.process(input_file)
-
-    data = table.getTfidfArray()
-
-    bs = BayesianSets.BayesianSets()
-    
-    # documents other than the relevant documents
-    index = [x for x in range(0,len(data)) if x not in query_index]
-
-    score = bs.score(data[query_index,:], data[index,:])
-
-    rank_index = np.argsort(np.multiply(score,-1))
-
-    offset_rank_index = [index[x] for x in rank_index]
-
-    # Get the urls corresponding to the scored documents
-    ranked_urls = table.getURLs(offset_rank_index)
-
-    for i in range(0,len(score)):
-        print ranked_urls[i], " ", score[rank_index[i]]
-
 if __name__=="__main__":
     main(sys.argv[1:])
