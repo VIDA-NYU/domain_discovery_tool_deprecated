7e4152aabe0df84fe3c23733e33592c6254c1b6c
yamuna <yamunak@gmail.com>
2015-03-06 13:54:44 -0500

    Added seedcrawler_api to integrate with sourcepin

---
+++ sourcepin_api/seed_crawler_model.py
@@ -0,0 +1,18 @@
+class seed_crawler_model:
+    def submit_query_terms(term_list):
+        return urls # results from Bing search
+        
+    
+    def submit_selected_urls(postive, negative):
+        # Test new positive and negative examples with exisitng classifier
+        # If accuracy above threshold classify pages
+        # Ranking 
+        # Diversification
+        
+        return urls # classified, ranked, diversified 
+
+    def extract_terms():
+        return terms
+
+    def submit_selected_terms(positive, negative):
+        return terms # ranked
