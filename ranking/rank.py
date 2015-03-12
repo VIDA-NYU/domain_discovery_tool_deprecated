ec4f75d50474ddf49c22f277a2bea29a0b74b5d3
yamuna <yamunak@gmail.com>
2015-03-12 03:00:13 -0400

    Modified to take list of positive and negative urls rather than just their indices

--- ranking/rank.py
+++ ranking/rank.py
@@ -5,32 +5,33 @@ import BayesianSets
 import numpy as np
 
 class rank:
-    def results(self,input_file,query_index,ignore_index):
+    def results(self,input_file,positive,negative):
+        documents = {}
+        other = []
+
+        with open(input_file,'r') as f:
+            for line in f.readlines():
+                url, content = line.strip().split(";")
+                if url not in negative:
+                    documents[url] = content
+                    if url not in positive:
+                        other.append(url)
+        
         table = tfidf.tfidf()
     
         # Compute tfidf of terms in the documents
-        adjusted_indices = table.process(input_file, ignore_index)
-        query_index_copy = query_index
-        
-        query_index = [adjusted_indices[x] for x in query_index_copy]
-        
-        data = table.getTfidfArray()
+        table.process(documents)
+
+        subquery_data = table.getTfidfArray(positive)
+        other_data = table.getTfidfArray(other)
 
         bs = BayesianSets.BayesianSets()
-    
-        # documents other than the relevant documents
-        index = [x for x in range(0,len(data)) if x not in query_index ]
+        
+        score = bs.score(subquery_data, other_data)
 
-        score = bs.score(data[query_index,:], data[index,:])
-
-        rank_index = np.argsort(np.multiply(score,-1))
-
-        offset_rank_index = [index[x] for x in rank_index]
-
-        # Get the urls corresponding to the scored documents
-        ranked_urls = table.getURLs(offset_rank_index)
-
-        ranked_scores = [score[rank_index[i]] for i in range(0, len(score))]
+        indices = np.argsort(np.multiply(score,-1))
+        ranked_urls = [other[index] for index in indices]
+        ranked_scores = [score[index] for index in indices]
         return [ranked_urls,ranked_scores]
 
 def main(argv):
