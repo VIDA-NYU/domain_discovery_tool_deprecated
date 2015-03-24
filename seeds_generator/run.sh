c50175489c8267f1b7314e1b19c38a5e542eb0cf
yamuna <yamunak@gmail.com>
2015-03-24 17:14:03 -0400

    removed inputs to BingSearch

--- seeds_generator/run.sh
+++ seeds_generator/run.sh
@@ -1,4 +1,4 @@
 sh compile.sh
-java -cp .:class:libs/commons-codec-1.9.jar BingSearch $1 $2 $3
+java -cp .:class:libs/commons-codec-1.9.jar BingSearch
 mkdir -p html
 python src/download.py results.txt html
