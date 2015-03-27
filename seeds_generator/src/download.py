eccfff05c9bcf787753de86a38984373f49ce59f
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-03-27 18:17:24 -0400

    Made the download async

--- seeds_generator/src/download.py
+++ seeds_generator/src/download.py
@@ -2,31 +2,54 @@
 # thread_download.py
 
 from multiprocessing import Pool
+from datetime import datetime
 import sys
 import urllib2
 import socket
 import traceback
 import requests
 
+from add_documents import add_document, extract_text
+
 from os import chdir, environ, getpid
 
 #processes is the list of urls from the input link
-    
-def encode(url):
+
+def encode( url):
   return urllib2.quote(url).replace("/", "%2F")
 
-def decode(url):
+def decode( url):
   return urllib2.unquote(url).replace("%2F", "/")
 
-def validate_url(url):
-    s = url[:4]
-    if s == "http":
-        return url
-    else:
-        url = "http://" + url
-        return url
+def validate_url( url):
+  s = url[:4]
+  if s == "http":
+    return url
+  else:
+    url = "http://" + url
+  return url
+  
 
-def download_one((given_url, outputdir)):
+def download( inputfile, outputdir, parallel=False):
+  if parallel == True:
+    print 'MULTIPROCESSING'
+    startProcesses(inputfile, outputdir)
+  else:
+    with open(inputfile) as lines:
+      for line in lines:
+        download_one((line, outputdir))
+        
+def startProcesses( inputfile, outputdir):
+  #multiprocessing :
+  print 'START PROCESSES ', inputfile, ' ', outputdir
+  urls = []
+  with open(inputfile) as lines:
+    urls = [(line,outputdir) for line in lines]
+  pool = Pool(processes=3)
+  print "Pool created"
+  pool.map_async(download_one, urls, callback=finished) 
+
+def download_one( (given_url, outputdir)):
   try:
     url = given_url.strip("\n")
     url = validate_url(url)
@@ -34,6 +57,15 @@ def download_one((given_url, outputdir)):
     src = res.text
     src = src.encode('utf-8')
     print 'GOOD\t' + url + ', PID=' + str(getpid())
+    doc = extract_text(src,url)
+    entry = {
+            'url': url,
+            'text': doc,
+            'fetched': datetime.now(),
+        }
+    entries = [entry]
+    add_document(entries)
+    
     encoded_url = encode(url)
     f = open(outputdir + "/" + encoded_url, "w")
     f.write(src)
@@ -46,40 +78,18 @@ def download_one((given_url, outputdir)):
     traceback.print_exc()
     print 'EXCEPTION' + "\t" + url
 
-def finished:
-  print "processing complete."
-
-def download(inputfile, outputdir, parallel=False):
-  if parallel == True:
-    pool = Pool(1)
-    pool.apply_async(start_processes,(inputfile, outputdir),callback=finished)
-  else:
-    with open(inputfile) as lines:
-      for line in lines:
-        download_one((line, outputdir))
-
-def startProcesses(inputfile, outputdir):
-  #multiprocessing :
-  urls = []
-  with open(inputfile) as lines:
-    urls = [(line,outputdir) for line in lines]
-  print urls
-  pool = Pool(processes=3)
-  print "Pool created"
-  pool.map(download_one,urls) 
-  pool.close()
-  pool.join()
+def finished(x):
+  print "Processing ", str(getpid()), " is Complete.",x
   
 def main(argv):
-    if len(argv) != 2:
-        print "Invalid arguments"
-        print "python download.py inputfile outputdir"
-        return
-    inputfile=argv[0]
-    outputdir=argv[1]
-    
-    startProcesses(inputfile, outputdir)
-
+  if len(argv) != 2:
+    print "Invalid arguments"
+    print "python download.py inputfile outputdir"
+    return
+  inputfile=argv[0]
+  outputdir=argv[1]
+  
+  startProcesses(inputfile, outputdir)
 
 if __name__=="__main__":
-    main(sys.argv[1:])
+  main(sys.argv[1:])
