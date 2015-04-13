11475177dd97551e5d9caa6070aa33aed75c69de
yamuna <yamunak@gmail.com>
2015-04-13 12:45:48 -0400

    Added code to upload static image files

--- seed_crawler_site/seed_crawler_site/urls.py
+++ seed_crawler_site/seed_crawler_site/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+import settings
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'seed_crawler_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^test_polls/', include('test_polls.urls')),
     url(r'^web_search/', include('web_search.urls')),
-    url(r'^thanks/', include('thanks.urls')),
     url(r'^admin/', include(admin.site.urls)),
-]
+#    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
