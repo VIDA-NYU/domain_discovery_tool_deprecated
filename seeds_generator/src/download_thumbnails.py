dad25e16f682c76da4074f91d6b7a66028badf37
yamuna <yamunak@gmail.com>
2015-04-27 10:52:24 -0400

    Download thumbnails

--- seeds_generator/src/download_thumbnails.py
+++ seeds_generator/src/download_thumbnails.py
@@ -3,12 +3,13 @@ from os import environ, chdir, path
 from subprocess import call
 import base64
 from add_documents import update_document
+import urllib2
 
 THUMBNAIL_DIMENSIONS = "100px*130px"
 THUMBNAIL_ZOOM = "0.20"
 
 def encode(url):
-  url.replace("/","_")
+  return urllib2.quote(url).replace("/","%2F").replace('%','')
 
 def startProcesses( inputfile):
   #multiprocessing :
@@ -26,35 +27,41 @@ def startProcesses( inputfile):
   print "Pool created"
   pool.map_async(download_thumnail, urls, callback=lambda x:finished_thumbnail(x,"Finished Processing")) 
 
-def download_thumbnail(url, updatedb=True):
+def download_thumbnail(url, download = True, updatedb=True):
   #Download thumbail
-  print 'Downloading thumnail for ', url
+  print 'Downloading thumbnail for ', url
   chdir(environ['MEMEX_HOME'] + "/phantomjs/")
   comm = "bin/phantomjs"
   paramJs = environ['MEMEX_HOME'] + "/phantomjs/examples/rasterize.js"
-  paramImg = environ['MEMEX_HOME'] + "/seed_crawler/seeds_generator/thumbnails/" + url.encode(url)+".png"
-  call([comm, paramJs, url, paramImg, THUMBNAIL_DIMENSIONS, THUMBNAIL_ZOOM])
+  imageFileName = encode(url)+".png"
+  imageFilePath = environ['MEMEX_HOME'] + "/seed_crawler/seeds_generator/thumbnails/"
+  paramImg = imageFilePath + imageFileName
+
+  if download:
+    call([comm, paramJs, url, paramImg, THUMBNAIL_DIMENSIONS, THUMBNAIL_ZOOM])
+
+  if not path.exists(paramImg):
+    imageFileName = "default_thumbnail.jpeg"
+    paramImg = imageFilePath + imageFileName
 
   if updatedb:
-    if path.exists(paramImg):
-      with open(paramImg, 'rb') as img:
-        e = {
-          "doc": {
-            "thumbnail": base64.b64encode(img.read())
-          }
+    with open(paramImg, 'rb') as img:
+      e = {
+        "doc": {
+          "thumbnail_name": imageFileName,
+          "thumbnail": base64.b64encode(img.read())
         }
-        update_document(url, e)
-        print "updated thumbnail for ", url
-    else:
-      print "No thumbnail downloaded"
+      }
+      update_document(url, e)
+      print "updated thumbnail for ", url
   
-def download_thumbnails(inputfile, parallel=False, updatedb=True):
+def download_thumbnails(inputfile, parallel=False, download=True, updatedb=True):
   if parallel:
     startProcesses(inputfile)
   else:
     with open(inputfile,'r') as f:
       for url in f:
-        download_thumbnail(url.strip(), updatedb)
+        download_thumbnail(url.strip(), download, updatedb)
 
 def main(argv):
   if len(argv) != 1:
@@ -62,7 +69,7 @@ def main(argv):
     print "python download.py inputfile"
     return
   inputfile=argv[0]
-  download_thumbnails(inputfile, updatedb=True)
+  download_thumbnails(inputfile, download=True, updatedb=True)
 
 if __name__=="__main__":
   main(sys.argv[1:])
