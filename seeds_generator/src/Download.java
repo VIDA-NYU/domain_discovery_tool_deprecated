bc4e54c26e8e02d7f9e346ee43ce241a41fd1ce2
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-05-05 18:50:55 -0400

    Moving download to java

---
+++ seeds_generator/src/Download.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.Executors;
+
+
+public class Download {
+
+    private String inputFile = "";
+    public void Download(String filename){
+	this.inputFile = filename;
+    }
+    
+    public void start(){
+	if(!"".equals(inputFile)){
+	    
+	}
+    }
+}
