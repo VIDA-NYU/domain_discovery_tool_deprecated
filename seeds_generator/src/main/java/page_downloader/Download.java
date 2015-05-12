60fa0b01a688504a07e9003ca6fd984848596e82
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-05-12 01:25:41 -0400

    Changed the constructor to also take the query as input Added awaitTermination to wait for the threads to finish within a specified tieme

--- seeds_generator/src/main/java/page_downloader/Download.java
+++ seeds_generator/src/main/java/page_downloader/Download.java
@@ -3,32 +3,41 @@ import java.util.concurrent.ExecutorService;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 
 public class Download {
 
     private String inputFile = "";
-    public Download(String filename){
+    private String query = "";
+    public Download(String filename, String query){
 	this.inputFile = filename;
+	this.query = query;
     }
 
     public void start(){
 	
 	int poolSize = Runtime.getRuntime().availableProcessors();
+	poolSize = 101;
 	ExecutorService downloaderService = Executors.newFixedThreadPool(poolSize-1);
-	
 	
 	if(!inputFile.equals("")){
 	    try {
-	
 		FileReader fr = new FileReader(this.inputFile); 
 		BufferedReader br = new BufferedReader(fr); 
 		String url; 
 		while((url = br.readLine()) != null) { 
-		    downloaderService.execute(new Download_URL(url.trim()));
+		    downloaderService.execute(new Download_URL(url.trim(), this.query));
 		} 
 		fr.close(); 
 		downloaderService.shutdown();
+		try {
+		    //downloaderService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+		    downloaderService.awaitTermination(60 , TimeUnit.SECONDS);
+		    Download_URL.close();
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
 	    } catch (IOException e) {
 		e.printStackTrace();
 	    } 
@@ -37,7 +46,9 @@ public class Download {
     }
     
     public static void main(String[] args){
-	Download d = new Download(args[0]);
+	Download d = new Download(args[0], args[1]);
 	d.start();
+	System.out.println("Download Finished");
+	System.exit(0);
     }
 }
