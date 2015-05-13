8af82644d4a810902f604b96b0f54b3df5eedb08
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-05-13 18:53:14 -0400

    Added stopwords removal and parts of speech tagging to filter unwanted words

--- elastic/get_mtermvectors.py
+++ elastic/get_mtermvectors.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python
 from elasticsearch import Elasticsearch
 from sklearn.feature_extraction import DictVectorizer
+import nltk
 import sys
 import pprint
 from os import environ
 import math
+from get_all_documents import get_all_documents
 
-es_server = 'http://localhost:9200/'
-if environ.get('ELASTICSEARCH_SERVER'):
-    es_server = environ['ELASTICSEARCH_SERVER']
-es = Elasticsearch(es_server)
-
-query = {
-    "query": {
-        "match_all": {}
-    }
-}
+ENGLISH_STOPWORDS = set(nltk.corpus.stopwords.words('english'))
 
 def tfidf(tf, df, n_docs):
     idf = math.log(n_docs / float(df))
@@ -25,37 +18,50 @@ def terms_from_es_json(doc):
     terms = {}
     docterms = doc["term_vectors"]["text"]["terms"]
     n_doc = doc["term_vectors"]["text"]["field_statistics"]["doc_count"]
-    for k, v in docterms.iteritems():
-        terms[k] = tfidf(v["term_freq"], v["doc_freq"], n_doc)
+    no_stopwords = [k for k in docterms.keys() if k not in ENGLISH_STOPWORDS and (len(k) > 2)]
+    #print no_stopwords, "\n"
+    tagged = nltk.pos_tag(no_stopwords)
+    #print tagged, "\n"
+    tags = ['NN', 'NNS', 'NNP', 'NNPS', 'VBN', 'JJ']
+    valid_words = [tag[0] for tag in tagged if tag[1] in tags]
+    #print valid_words, "\n"
+    for k in valid_words:
+        terms[k] = tfidf(docterms[k]["term_freq"], docterms[k]["doc_freq"], n_doc)
     return terms
 
-res = es.search(body=query, index='memex', doc_type='page',fields=[],scroll='1m',search_type='scan', size=100)
-all_hits = res['hits']['hits']
-tfidfs = []
-docs = []
+def getTermStatistics(all_hits):
+ 
+    host =  environ['ELASTICSEARCH_SERVER'] if environ.get('ELASTICSEARCH_SERVER') else 'localhost'
+    es = Elasticsearch(hosts=[host])
+    tfidfs = []
+    docs = []
 
-#pprint.pprint(res)
-print 'Document found: %d' % res['hits']['total']
-while '_scroll_id' in res:
-    res = es.scroll(scroll_id=res['_scroll_id'])
-#    pprint.pprint(res)
-    hits = [hit['_id'] for hit in res['hits']['hits']]
-    all_hits += hits
+    for i in range(0, len(all_hits), 100):
+        hits = all_hits[i:i+100]
+        term_res = es.mtermvectors(index=environ['ELASTICSEARCH_INDEX'] if environ.get('ELASTICSEARCH_INDEX') else 'memex', 
+                                   doc_type=environ['ELASTICSEARCH_DOC_TYPE'] if environ.get('ELASTICSEARCH_DOC_TYPE') else 'page', 
+                                   term_statistics=True, fields=['text'], ids=hits)
+        #pprint.pprint(term_res['docs'])
+        for doc in term_res['docs']:
+            #pprint.pprint(doc)
+            if doc.get('term_vectors'):
+                if 'text' in doc['term_vectors']:
+                    docs.append(doc['_id'])
+                    tfidfs.append(terms_from_es_json(doc))
+            #else:
+             #   pprint.pprint(doc)
+        #pprint.pprint(tfidfs)
+    
+    v = DictVectorizer()
 
+    return [v.fit_transform(tfidfs), v.get_feature_names()]
 
-for i in range(0, len(all_hits), 100):
-    hits = all_hits[i:i+100]
-    term_res = es.mtermvectors(index='memex', doc_type='page', term_statistics=True, fields=['text'], ids=hits)
-    #    pprint.pprint(term_res['docs'])
-    for doc in term_res['docs']:
-        #pprint.pprint(doc)
-        if 'text' in doc["term_vectors"]:
-            docs.append(doc['_id'])
-            tfidfs.append(terms_from_es_json(doc))
-
-#pprint.pprint(tfidfs)
-
-v = DictVectorizer()
-X_tfidf = v.fit_transform(tfidfs)
-
-pprint.pprint(X_tfidf)
+if __name__ == "__main__":
+    
+    res = get_all_documents(fields = ["url"])
+    #pprint.pprint(res)
+    
+    all_hits = [hit['_id'] for hit in res]
+    #pprint.pprint(all_hits)
+    [_, features] = getTermStatistics(all_hits)
+    print features
