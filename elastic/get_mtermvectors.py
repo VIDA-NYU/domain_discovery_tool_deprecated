c9f8cee347dd9117ef69baa10c1e4d171e4220e8
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-05-15 16:49:16 -0400

    removed erroneous imports fixed typo in variable name return the features along with the tfidfs

--- elastic/get_mtermvectors.py
+++ elastic/get_mtermvectors.py
@@ -3,19 +3,29 @@ from sklearn.feature_extraction import DictVectorizer
 import sys
 import pprint
 import math
+import os
 
-from elastic import *
-from .generators import *
+from elasticsearch import Elasticsearch
+from generators import *
 
 def get_tfidfs():
+    es_server = 'http://localhost:9200/'
+    if os.environ.get('ELASTICSEARCH_SERVER'):
+        es_server = os.environ['ELASTICSEARCH_SERVER']
+    es = Elasticsearch(es_server)
+    
     ids = ids_generator(es, index='memex', doc_type='page')
     tfidfs = []
     docs = []
     for (id, term_vector) in termvectors_generator(es, index='memex', doc_type='page', ids_generator=ids, fields=['text'], size=10):
-        tfids.append(term_vector)
+        tfidfs.append(term_vector)
         docs.append(id)
 
     v = DictVectorizer()
     X_tfidf = v.fit_transform(tfidfs)
-    return X_tfidf
+    Vocab = v.get_feature_names()
+    return [X_tfidf, Vocab]
+
+if __name__ == "__main__":
+    print get_tfidfs()
 
