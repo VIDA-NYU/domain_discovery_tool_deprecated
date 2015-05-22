deb745a71e0207682573700b837b4507625549d3
Cesar Morais Palomo <cesarpalomo@gmail.com>
2015-05-22 11:03:16 -0400

    reverted to correct version to fix an introduced bug

--- vis/server.py
+++ vis/server.py
@@ -29,7 +29,7 @@ class Page:
   # Default constructor reading app config file.
   def __init__(self):
     # Folder with html content.
-    self._HTML_DIR = os.path.join(os.path.abspath("."), u"vis", u"html")
+    self._HTML_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), u"html")
 
 
   # Access to topics visualization.
