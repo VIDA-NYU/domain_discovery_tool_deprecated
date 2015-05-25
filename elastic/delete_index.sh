94154d6faab4498254b8b22821256ad304c2da9c
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-05-25 19:29:43 -0400

    Take the index and elastic host as parameters

--- elastic/delete_index.sh
+++ elastic/delete_index.sh
@@ -1,1 +1,17 @@
-curl -XDELETE 'http://localhost:9200/memex/'; echo
+#!/bin/sh
+if [ $# -eq 0 ]
+then 
+    INDEX=memex
+else
+    INDEX=$1
+fi
+
+if [ $# -gt 1 ]
+then
+    ELASTIC=$2
+else
+    ELASTIC=http://localhost:9200
+fi
+echo $INDEX
+
+curl -XDELETE "$ELASTIC/$INDEX/"; echo
