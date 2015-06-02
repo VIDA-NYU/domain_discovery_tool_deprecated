365fa57f9a75e01eb3a47c9dbb69ea332164d192
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-06-02 17:56:19 -0400

    Added get_all_ids

--- elastic/get_documents.py
+++ elastic/get_documents.py
@@ -84,6 +84,26 @@ def get_most_recent_documents(opt_maxNumberOfPages = 1000, fields = [], opt_filt
 
     return results
 
+def get_all_ids(es_index = 'memex', es_doc_type = 'page', es = None):
+    if es is None:
+        es = ElasticSearch('http://localhost:9200')
+
+    query = {
+        "query": {
+            "match_all": {}
+        },
+        "fields": ['url']
+    }
+    
+    res = es.search(query, index = es_index, doc_type = es_doc_type, size = 100000)
+    hits = res['hits']['hits']
+    
+    urls = []
+    for hit in hits:
+        urls.append(hit['fields']['url'][0])
+
+    return urls
+
 if __name__ == "__main__":
     urls = []
     with open(environ['MEMEX_HOME']+'/seed_crawler/seeds_generator/results.txt', 'r') as f:
