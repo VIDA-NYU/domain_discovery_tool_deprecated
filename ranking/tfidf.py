af60b8c05ed4a269aaac349d5e87400dc014c5c7
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-06-04 10:40:25 -0400

    Added total term frequency

--- ranking/tfidf.py
+++ ranking/tfidf.py
@@ -8,12 +8,14 @@ from collections import OrderedDict
 from elastic.get_mtermvectors import getTermStatistics
 
 class tfidf:
-    def __init__(self, opt_docs = None):
+    def __init__(self, opt_docs = None, es_index = 'memex', es_doc_type = 'page', es = None):
         self.documents = opt_docs
         self.corpus = None
         self.tfidfArray = None
+        self.tfArray = None
+        self.ttf = None
         if opt_docs != None:
-          self.process(opt_docs)
+          self.process(opt_docs, es_index, es_doc_type, es)
 
     def getTopTerms(self,top):
         N = len(self.documents)
@@ -22,16 +24,21 @@ class tfidf:
         return [self.corpus[i] for i in sortedAvgIndices[0:top]]
 
     def getIndex(self, terms):
-        print "TERMS = ", terms
+
         index = []
         for term in terms:
             if term.strip() in self.corpus:
                 index.append(self.corpus.index(term.strip()))
-        print "INDICES = ", index
         return index
 
     def getTfidfArray(self):
-        return [self.documents, self.corpus, self.tfidfArray.toarray()]
+        return [self.documents, self.corpus, self.tfidfArray]
+
+    def getTfArray(self):
+        return [self.documents, self.corpus, self.tfArray]
+
+    def getTtf(self):
+        return self.ttf
 
     def getURLs(self, args):
         return self.documents
@@ -39,7 +46,9 @@ class tfidf:
     def getTerms(self, indices):
         return [self.corpus[x] for x in indices]
 
-    def process(self, documents):
-        [data, corpus] = getTermStatistics(documents)
-        self.tfidfArray = data
+    def process(self, documents, es_index = 'memex', es_doc_type = 'page', es = None):
+        [data_tfidf, data_tf, data_ttf, corpus] = getTermStatistics(documents, es_index, es_doc_type, es)
+        self.tfidfArray = data_tfidf
+        self.tfArray = data_tf
+        self.ttf = data_ttf
         self.corpus = corpus
