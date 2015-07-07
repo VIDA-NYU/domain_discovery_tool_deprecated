0cffb8803615a47e7817cb269ea1506c33f6385e
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-07-06 23:46:13 -0400

    creates new crawler index and adds the corresponding mappings

---
+++ elastic/create_index.py
@@ -0,0 +1,28 @@
+from elasticsearch import Elasticsearch
+from os import environ
+import json
+
+from config import es as default_es
+
+def create_index(es_index='memex', es=None):
+    if es is None:
+        es = default_es
+
+    json_page_data=open(environ['DDT_HOME']+'/elastic/mapping.json').read()
+    json_terms_data=open(environ['DDT_HOME']+'/elastic/mapping_terms.json').read()
+
+    page_mappings = json.loads(json_page_data)
+    terms_mappings = json.loads(json_terms_data)
+
+    mappings = {"mappings": 
+                {
+                    "page": page_mappings["page"], 
+                    "terms":terms_mappings["terms"]
+                }
+            }
+    
+    es_index = '_'.join(es_index.lower().split(' '))
+
+    res = es.indices.create(index=es_index, body=mappings, ignore=400)
+    
+    return res
