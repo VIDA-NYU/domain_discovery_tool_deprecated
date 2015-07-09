a8228fbfc2920699e14e82ecab4a58ca4e2da46e
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-07-09 15:47:33 -0400

    When new crawler is added do not select it in the crawler list just update the list with the new crawler

--- vis/html/js/crawlervis.js
+++ vis/html/js/crawlervis.js
@@ -269,29 +269,9 @@ CrawlerVis.prototype.reloadSelectForAvailableCrawlers = function(data) {
 	      return d.name + ' (' + Utils.parseFullDate(d.creation) + ')';
 	  });
       
-      $(document).ready(function() {
-	  // Generate the index name from the entered crawler name
-	  var index_name = d3.select('#crawler_index_name').node().value;
-	  var words = index_name.toLowerCase().split(' ');
-	  for(var i = words.length; i--;) {
-              if(words[i] === '') {
-		  words.splice(i, 1);
-              }
-	  }
-	  var crawlerId = words.join("_");
-	  $("#selectCrawler option[value="+crawlerId+"]").prop('selected', true);
-      });
-      
       // Generate the index name from the entered crawler name
       var index_name = d3.select('#crawler_index_name').node().value;
-      var words = index_name.toLowerCase().split(' ');
-      for(var i = words.length; i--;) {
-          if(words[i] === '') {
-              words.splice(i, 1);
-          }
-      }
-      var crawlerId = words.join("_");
-      vis.setActiveCrawler(crawlerId);
+
       document.getElementById("status_panel").innerHTML = 'Added new crawler - ' + index_name;
 
   } else  document.getElementById("status_panel").innerHTML = 'No crawlers found'
