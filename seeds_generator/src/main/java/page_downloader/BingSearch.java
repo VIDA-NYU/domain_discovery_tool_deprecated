4f683168eb576dc7275b1b20fcf790291797a76d
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-07-09 15:08:08 -0400

    Merged Bing serach and page downloads

--- seeds_generator/src/main/java/page_downloader/BingSearch.java
+++ seeds_generator/src/main/java/page_downloader/BingSearch.java
@@ -60,27 +60,31 @@ public class BingSearch {
 	return url;
     }
 	
-    public ArrayList<String> search(String query, String top){
+    public ArrayList<String> search(String query, String top, String es_index, String es_doc_type, String es_server){
 	System.out.println("Query: " + query);
 
 	if (this.prop == null){
 	    System.out.println("Error: config file is not loaded yet");
 	    return null;
 	}
+
+	Download download = new Download(query, es_index, es_doc_type, es_server);
+	
 	ArrayList<String> results = new ArrayList<String>();
 	query = query.replaceAll(" ", "%20");
 	byte[] accountKeyBytes = Base64.encodeBase64((this.accountKey + ":" + this.accountKey).getBytes());
 	String accountKeyEnc = new String(accountKeyBytes);
-	URL url;
+	URL query_url;
 	try {
 	    int chunk = 50;
 	    if (Integer.valueOf(top) < 50)
 		chunk = Integer.valueOf(top); 
 	    int skip_index = 0;
 	    while(chunk > 0){
-	    	url = new URL("https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Web?$skip=" + String.valueOf(skip_index*50) + "&Query=%27" + query + "%27&$top=" + String.valueOf(chunk));
-	    	System.out.println(url);
-	    	HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+	    	query_url = new URL("https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Web?$skip=" + String.valueOf(skip_index*50) + "&Query=%27" + query + "%27&$top=" + String.valueOf(chunk));
+	    	System.out.println(query_url);
+
+	    	HttpURLConnection conn = (HttpURLConnection)query_url.openConnection();
 	    	conn.setRequestMethod("GET");
 	    	conn.setRequestProperty("Authorization", "Basic " + accountKeyEnc);
 
@@ -102,7 +106,9 @@ public class BingSearch {
 	    	for (int i=0; i<totalUrls; i++){
 			Element e = (Element)urls.item(i);
 			NodeList nl = e.getChildNodes();
-			results.add(validate_url((nl.item(0).getNodeValue())));
+			String url = validate_url((nl.item(0).getNodeValue()));
+			results.add(url);
+			download.addTask(url);
 	    	}
 		if ((Integer.valueOf(top) - chunk) < 50) 
 			chunk = Integer.valueOf(top) - chunk;
@@ -119,52 +125,45 @@ public class BingSearch {
 	catch (Exception e){
 	    e.printStackTrace();
 	}
+
+	download.shutdown();
 	System.out.println("Number of results: " + String.valueOf(results.size()));
 	return results;
     }
 
     public static void main(String[] args) {
-
+	
 	String queryfile = "conf/queries.txt"; //default
-	String outputfile = "results.txt";//default
 	String top = "50"; //default
-      
+	String es_index = "memex";
+	String es_doc_type = "page";
+	String es_server = "localhost";
+	
 	int i = 0;
 	while (i < args.length){
 	    String arg = args[i];
 	    if(arg.equals("-q")){
 		queryfile = args[++i];
-	    }
-	    else if(arg.equals("-o")){
-		outputfile = args[++i];
 	    } else if(arg.equals("-t")){ 
 		top = args[++i];
-	    }
-	    else {
+	    } else if(arg.equals("-i")){
+		es_index = args[++i];
+	    } else if(arg.equals("-d")){
+		es_doc_type = args[++i];
+	    } else if(arg.equals("-s")){
+		es_server = args[++i];
+	    }else {
 		System.out.println("Unrecognized option");
 		break;
 	    }
 	    ++i;
 	}
-      
+	
 	System.out.println("Get the top " + top + " results");
-      
+	
 	BingSearch bs = new BingSearch();
-	ArrayList<String> results = new ArrayList<String>();
 	ArrayList<String> queries = bs.read_queries(queryfile);
-	for(String query : queries){
-	    results.addAll(bs.search(query, top));
-	}
-      
-	try{
-	    PrintWriter writer = new PrintWriter(outputfile, "UTF-8");
-	    for (String result : results){
-		writer.println(result);
-	    }
-	    writer.close();
-	}
-	catch(Exception e){
-	    e.printStackTrace();
-	}
-    }        
+	for(String query: queries)
+	    bs.search(query, top, es_index, es_doc_type, es_server);
+    }
 }
--- seeds_generator/src/main/java/page_downloader/Download.java
+++ seeds_generator/src/main/java/page_downloader/Download.java
@@ -10,59 +10,49 @@ import org.elasticsearch.client.Client;
 
 public class Download {
 
-    private String inputFile = "";
     private String query = "";
     private String es_index = "memex";
     private String es_doc_type = "page";
     private Client client = null;
+    private int poolSize = 100;
+    private ExecutorService downloaderService = Executors.newFixedThreadPool(poolSize);
 
-    public Download(String filename, String query, String es_index, String es_doc_type, String es_host){
-	this.inputFile = filename;
+    public Download(String query, String es_index, String es_doc_type, String es_host){
 	this.query = query;
 	if(es_host.isEmpty())
 	    es_host = "localhost";
-	this.client = new TransportClient().addTransportAddress(new InetSocketTransportAddress(es_host, 9300));
+	else {
+	    String[] parts = es_host.split(":");
+	    if (parts.length == 2)
+		es_host = parts[0];
+	    else if(parts.length == 3)
+		es_host = parts[1];
+	    
+	    es_host = es_host.replaceAll("/","");
+	}
 
+	this.client = new TransportClient().addTransportAddress(new InetSocketTransportAddress(es_host, 9300));
+	
 	if(!es_index.isEmpty())
 	    this.es_index = es_index;
 	if(!es_doc_type.isEmpty())
 	    this.es_doc_type = es_doc_type;
     }
 
-    public void start(){
-	
-	int poolSize = Runtime.getRuntime().availableProcessors();
-	poolSize = 101;
-	ExecutorService downloaderService = Executors.newFixedThreadPool(poolSize-1);
-	
-	if(!inputFile.equals("")){
-	    try {
-		FileReader fr = new FileReader(this.inputFile); 
-		BufferedReader br = new BufferedReader(fr); 
-		String url; 
-		while((url = br.readLine()) != null) { 
-		    downloaderService.execute(new Download_URL(url.trim(), this.query, this.es_index, this.es_doc_type, this.client));
-		} 
-		fr.close(); 
-		downloaderService.shutdown();
-		try {
-		    //downloaderService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-		    downloaderService.awaitTermination(60 , TimeUnit.SECONDS);
-		    this.client.close();
-		} catch (InterruptedException e) {
-		    e.printStackTrace();
-		}
-	    } catch (IOException e) {
-		e.printStackTrace();
-	    } 
-	}
-
+    public void addTask(String url){
+	downloaderService.execute(new Download_URL(url.trim(), this.query, this.es_index, this.es_doc_type, this.client));
     }
     
-    public static void main(String[] args){
-	Download d = new Download(args[0], args[1], args[2], args[3], args[4]);
-	d.start();
-	System.out.println("Download Finished");
-	System.exit(0);
+    public void shutdown(){
+	try {
+	    downloaderService.shutdown();
+	    //downloaderService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+	    downloaderService.awaitTermination(60 , TimeUnit.SECONDS);
+	    this.client.close();
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
     }
+
+    
 }
