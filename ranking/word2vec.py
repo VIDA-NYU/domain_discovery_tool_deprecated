17ea9303dc1749a82cc83e11e9c4867b6917f381
Yamuna Krishnamurthy <yamuna@nyu.edu>
2015-07-12 16:36:13 -0400

    replace getTermStatistics with getTermFrequency to increase performance

--- ranking/word2vec.py
+++ ranking/word2vec.py
@@ -3,39 +3,44 @@ import numpy as np
 from os import environ
 from pprint import pprint
 
-from elastic.get_mtermvectors import getTermStatistics
+from elastic.get_mtermvectors import getTermFrequency
 
 class word2vec:
     def __init__(self, opt_docs = None, es_index = 'memex', es_doc_type = 'page', es = None):
         self.documents = opt_docs
         self.word2vec = None
 
+        f = open(environ['DDT_HOME']+'/ranking/D_cbow_pdw_8B.pkl', 'rb')
+        self.word_vec = load(f)
+
         if opt_docs != None:
             self.process(opt_docs, es_index, es_doc_type, es)
 
     def get_word2vec(self):
         return [self.documents,self.word2vec]
+
+    def get(self, word):
+        return self.word_vec.get(word)
         
     def process(self, documents, es_index = 'memex', es_doc_type = 'page', es = None):
 
-        [data_tfidf, data_tf, data_ttf, corpus, urls] = getTermStatistics(documents, es_index, es_doc_type, es)
+        [data_tf, corpus, urls] = getTermFrequency(documents, self.word_vec, es_index, es_doc_type, es)
         
-        f = open(environ['DDT_HOME']+'/ranking/D_cbow_pdw_8B.pkl', 'rb')
-        word_vec = load(f)
-        
+        documents = urls
+
         word2vec_list_docs = []
-        data_tf_array = data_tf.toarray()
         urls = []
-        for i in range(0,np.shape(data_tf_array)[0]):
-            word_tf_doc = data_tf_array[i]
-            word_vec_doc = [word_vec[corpus[j]] for j in range(0,len(word_tf_doc)) if word_tf_doc[j] > 5 and not word_vec.get(corpus[j]) is None]
+        i = 0
+        for doc in data_tf:
+            word_vec_doc = [self.word_vec[term] for term in doc.keys() if doc[term] > 5 and not self.word_vec.get(term) is None]
             if word_vec_doc:
                 m_word_vec = np.array(word_vec_doc).mean(axis=0) 
                 word2vec_list_docs.append(m_word_vec.tolist())
-                urls.append(self.documents[i])
+                urls.append(documents[i])
+                i = i + 1
         
         self.documents = urls
         
         self.word2vec = np.array(word2vec_list_docs)
-        
+        return [self.documents,self.word2vec]
         
