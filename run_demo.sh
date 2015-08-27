a8905e9a900530c1ed04560f04d2202bf93aa207
Aecio Santos <aecio.solando@gmail.com>
2015-08-26 21:37:27 -0400

    Change Dockerfile to use new Makefile

--- run_demo.sh
+++ run_demo.sh
@@ -1,18 +1,8 @@
-#!/bin/sh
-if [ -z "$ELASTICSEARCH_SERVER" ]; then
-	#start elasticsearch and wait elasticsearch bind to port
-	service elasticsearch start
-	ELASTICSEARCH_SERVER=http://localhost:9200
-	echo "Waiting ElasticSearch start..."
-	while netstat -lnt | awk '$4 ~ /:9200$/ {exit 1}'; do sleep 1; done
-else
-	sleep 5
-fi
+#!/bin/bash
+echo "Activating DDT enviroment..."
+source activate ddt
 
 echo "Using ElasticSearch at $ELASTICSEARCH_SERVER"
 
-# run server
-echo "Running server..."
-cd /domain_discovery_tool
-fab runvis
-#fab runvis >> ./ddt.log 2>&1 &
+echo "Starting services..."
+supervisord -c /ddt/supervisord.conf
