88d014db4afa207cfc7c43528e5ac86b81757a95
Brittain Hard <brittainhard@gmail.com>
2016-01-21 14:50:14 -0600

    Improved wording on statistics dashboard.

--- vis/bokeh_plots/domains_dashboard.py
+++ vis/bokeh_plots/domains_dashboard.py
@@ -14,7 +14,12 @@ from bokeh.charts import Bar
 from bokeh.io import vform, vplot
 
 
-PLOT_ELEMENTS = 10
+DOMAIN_PLOT_LIMIT = 10
+DOMAIN_TABLE_LIMIT = None
+
+ENDING_PLOT_LIMIT = 10
+ENDING_TABLE_LIMIT = None
+
 BAR_WIDTH = 0.4
 
 
@@ -38,41 +43,52 @@ def domains_dashboard(response, extra_plots=None):
     parsed_urls = [urlparse(x).hostname for x in urls]
 
     # Domain names Bar chart.
-    domains = Counter(parsed_urls).most_common(PLOT_ELEMENTS)
-    xdomains = [x[0] for x in domains]
-    ydomains = [y[1] for y in domains]
-
+    domains_counter = Counter(parsed_urls).most_common(DOMAIN_PLOT_LIMIT)
+    xdomains = [x[0] for x in domains_counter]
+    ydomains = [y[1] for y in domains_counter]
     source_domains = ColumnDataSource(data=dict(x=xdomains, y=ydomains))
-    bar_domains = Bar(source_domains.data, values="y", label="x", title="Most Common Domains by Number",
-            bar_width=BAR_WIDTH, height=584, xlabel="Domains",
+
+    bar_domains = Bar(source_domains.data, values="y", label="x", title="Most Common Sites by Number",
+            bar_width=BAR_WIDTH, height=584, xlabel="Sites",
             ylabel="Occurences")
-    panel_domains = Panel(child=bar_domains, title="Domains")
+    panel_domains = Panel(child=bar_domains, title="Sites")
 
     # Domain Information Table
+    table_domains_counter = Counter(parsed_urls).most_common(DOMAIN_TABLE_LIMIT)
+    xdomains_table = [x[0] for x in table_domains_counter]
+    ydomains_table = [y[1] for y in table_domains_counter]
+    source_table_domains = ColumnDataSource(data=dict(x=xdomains_table,
+        y=ydomains_table))
+
     columns_domain = [
-            TableColumn(field="x", title="Domain"),
+            TableColumn(field="x", title="Site Name"),
             TableColumn(field="y", title="Count"),
         ]
-    data_table_domain = DataTable(source=source_domains, columns=columns_domain, width=400,
+    data_table_domain = DataTable(source=source_table_domains, columns=columns_domain, width=400,
             height=280)
 
     # Top level Domains Bar Chart
-    endings = Counter([x[x.rfind("."):] for x in parsed_urls]).most_common(PLOT_ELEMENTS)
-    xendings = [x[0] for x in endings]
-    yendings = [y[1] for y in endings]
-
+    endings_counter = Counter([x[x.rfind("."):] for x in parsed_urls]).most_common(ENDING_PLOT_LIMIT)
+    xendings = [x[0] for x in endings_counter]
+    yendings = [y[1] for y in endings_counter]
     source_top_level = ColumnDataSource(data=dict(x=xendings, y=yendings))
+
     bar_top_level = Bar(source_top_level.data, values="y", label="x",
             title="Most Common URL Endings by Number", bar_width=BAR_WIDTH,
             height=584, xlabel="Endings", ylabel="Occurences")
     panel_top_level = Panel(child=bar_top_level, title="Endings")
 
     # Top level domains table
+    table_endings_counter = Counter([x[x.rfind("."):] for x in parsed_urls]).most_common(ENDING_TABLE_LIMIT)
+    xendings_table = [x[0] for x in table_endings_counter]
+    yendings_table = [y[1] for y in table_endings_counter]
+    source_table_top_level = ColumnDataSource(data=dict(x=xendings_table, y=yendings_table))
+
     columns_top_level = [
-            TableColumn(field="x", title="Top Level Domain"),
+            TableColumn(field="x", title="Ending"),
             TableColumn(field="y", title="Count"),
         ]
-    data_table_top_level = DataTable(source=source_top_level,
+    data_table_top_level = DataTable(source=source_table_top_level,
             columns=columns_top_level, width=400, height=280)
 
     # Add the plots and charts to a vform and organize them with VBox and HBox
