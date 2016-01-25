06df767d9ca6989a72f0d7ac389d42aaecb1d73b
Brittain Hard <brittainhard@gmail.com>
2016-01-25 13:12:39 -0600

    Working buttons, default settings only.

--- vis/html/js/topicvis.js
+++ vis/html/js/topicvis.js
@@ -5,6 +5,16 @@
     vectorizer: "bag_of_words",
     model: "plsa",
     ntopics: 2,
+    visualizer: "",
+    domain: "",
   }
+
+
+  $("#ldavisPlot, #termitePlot").on("click", function(){
+    exports.visSettings.visualizer = $(this).attr("value");
+    exports.visSettings.domain = $('input[name="crawlerRadio"]:checked').attr("placeholder").toLowerCase()
+    var url = "/topicsvis?" + $.param(exports.visSettings);
+    $(this).attr("href", url);
+  });
 
 })(this.TopikVis = {});
