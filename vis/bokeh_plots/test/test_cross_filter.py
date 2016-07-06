abd564e6f6a35e47a5f83af10e3a374a97dcc576
Luke Canavan <canavandl@gmail.com>
2016-07-05 22:42:45 -0500

    Update plots with new cross filtering code

--- vis/bokeh_plots/test/test_cross_filter.py
+++ vis/bokeh_plots/test/test_cross_filter.py
@@ -32,15 +32,20 @@ def es_response():
 
 def test_parse_es_response(es_response):
     # note that data becomes ordered by `retrieved` field
-    data = {'hostname': ['reuters.com', 'nytimes.com', 'politico.com', 'applevacations.com', 'bananavacations.com'],
-            u'query': [['apple'], ['carrot'], ['apple', 'banana'], ['apple', 'banana'], ['banana']],
-            u'tag': [['Untagged'], ['Untagged'], ['Relevant'], ['Irrelevant', 'Relevant'], ['Irrelevant', 'Relevant']],
-            'tld': [u'.com', u'.com', u'.com', u'.com', u'.com'],
-            'url': ['http://www.reuters.com/article/us-apple-encryption-hearing-idUSKCN0XB2RU',
-                    'http://www.nytimes.com/article/us-apple-encryption-hearing-idUSKCN0XB2RU',
-                    'http://www.politico.com/story/2016/04/apple-hires-cynthia-hogan-221937',
-                    'http://www.applevacations.com/',
-                    'http://www.bananavacations.com/']}
+    data = {'url': [u'http://www.reuters.com/article/us-apple-encryption-hearing-idUSKCN0XB2RU',
+             u'http://www.nytimes.com/article/us-apple-encryption-hearing-idUSKCN0XB2RU',
+             u'http://www.politico.com/story/2016/04/apple-hires-cynthia-hogan-221937',
+             u'http://www.politico.com/story/2016/04/apple-hires-cynthia-hogan-221937',
+             u'http://www.applevacations.com/',
+             u'http://www.applevacations.com/',
+             u'http://www.applevacations.com/',
+             u'http://www.applevacations.com/',
+             u'http://www.bananavacations.com/',
+             u'http://www.bananavacations.com/'],
+     'query': [u'apple', u'carrot', u'apple', u'banana', u'apple', u'apple', u'banana', u'banana', u'banana', u'banana'],
+     'tag': ['Untagged', 'Untagged', u'Relevant', u'Relevant', u'Irrelevant', u'Relevant', u'Irrelevant', u'Relevant', u'Irrelevant', u'Relevant'],
+     'hostname': [u'reuters.com', u'nytimes.com', u'politico.com', u'politico.com', u'applevacations.com', u'applevacations.com', u'applevacations.com', u'applevacations.com', u'bananavacations.com', u'bananavacations.com'],
+     'tld': [u'.com', u'.com', u'.com', u'.com', u'.com', u'.com', u'.com', u'.com', u'.com', u'.com']}
 
     df = parse_es_response(es_response)
 
@@ -49,31 +54,31 @@ def test_parse_es_response(es_response):
 
 def test_calculate_query_correlation(es_response):
     df = parse_es_response(es_response)
-    df2 = duplicate_multi_rows(df, 'query')
 
-    graph = calculate_query_correlation(df2, 'query')
+    graph = calculate_query_correlation(df, 'query')
 
     assert graph == {(u'apple', u'banana'): 1.0}
 
 def test_calculate_graph_coords(es_response):
     df = parse_es_response(es_response)
-    df2 = duplicate_multi_rows(df, 'query')
 
-    graph = calculate_graph_coords(df2, 'query')
+    graph = calculate_graph_coords(df, 'query')
 
     assert np.allclose(graph.x.tolist(), [-0.5, -0.5, 1.0])
     assert np.allclose(graph.y.tolist(), [0.8660254037, -0.8660254037, 0.0])
-    assert graph.url.tolist() == [3, 3, 1]
+    assert graph.url.tolist() == [4, 5, 1]
     assert graph.index.tolist() == [u'apple', u'banana', u'carrot']
 
 def test_duplicate_multitag_rows(es_response):
     df = parse_es_response(es_response)
 
-    df2 = duplicate_multi_rows(df, 'tag')
+    # Refactored to be called inside of parse_es_response
+    # df = duplicate_multi_rows(df, 'tag')
 
-    assert df2.shape == (7,5)
-    assert df2.tag.tolist() == ["Untagged", "Untagged", u'Relevant',
-                                u'Irrelevant', u'Relevant', u'Irrelevant', u'Relevant']
+    assert df.shape == (10,5)
+    assert df.tag.tolist() == ['Untagged', 'Untagged', u'Relevant', u'Relevant',
+                               u'Irrelevant', u'Relevant', u'Irrelevant',
+                               u'Relevant', u'Irrelevant', u'Relevant']
 
 def test_normalize():
     assert np.allclose(normalize(pd.Series([1,2,3]), 3, 1.5).tolist(), [1.5, 2.0, 3.0])
