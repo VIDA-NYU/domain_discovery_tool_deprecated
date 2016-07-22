c7d166ed421a1b48d3a91a97f30c2bf7b1bffcf4
Yamuna Krishnamurthy <yamuna@nyu.edu>
2016-07-22 10:14:32 -0400

    Restrict bigrams to top MAX_PHRASES

--- ranking/get_bigrams_trigrams.py
+++ ranking/get_bigrams_trigrams.py
@@ -5,11 +5,14 @@ from sklearn.feature_extraction import DictVectorizer
 import numpy as np
 import operator
 import math
+from sets import Set
 
 from nltk import corpus
 ENGLISH_STOPWORDS = set(corpus.stopwords.words('english'))
 
 from pprint import pprint
+
+MAX_PHRASES = 1000
 
 def remove_stopword_phrases(phrases):
         selected_phrases = []
@@ -20,7 +23,7 @@ def remove_stopword_phrases(phrases):
         return selected_phrases
 
 def get_bigrams_trigrams(text=[], urls=[], termCount=20, w2v=None, es=None):
-        
+
         bigram_vectorizer = CountVectorizer(ngram_range=(2,2))
         bigram_analyze = bigram_vectorizer.build_analyzer()
         trigram_vectorizer = CountVectorizer(ngram_range=(3,3))
@@ -53,15 +56,38 @@ def get_bigrams_trigrams(text=[], urls=[], termCount=20, w2v=None, es=None):
                                 bi_dict_df[phrase] = bi_dict_df[phrase] + 1
                         else:
                                 bi_dict_df[phrase] = 1
+
+        if len(bi_dict_corpus) > MAX_PHRASES:
+                sorted_phrases = sorted(bi_dict_corpus.items(), key=operator.itemgetter(1), reverse=True)
+                bi_dict_corpus = {item[0]: bi_dict_corpus[item[0]] for item in sorted_phrases[0:MAX_PHRASES]}
+                bi_dict_df = {item[0]: bi_dict_df[item[0]] for item in sorted_phrases[0:MAX_PHRASES]}
         
         bigrams_tfidf = []
         for bigram in bigrams:
                 bigrams_dict_tfidf = {}
-                for phrase in bigram.keys():
-                        if bi_dict_df[phrase] > 0: #(len(bigrams)/3):
-                                bigrams_dict_tfidf[phrase] = bigram[phrase] * (math.log(len(bigrams)/ float(1+bi_dict_df[phrase])))
+                for phrase in bi_dict_corpus.keys():
+                        try:
+                                if bi_dict_df[phrase] > 0: #(len(bigrams)/3):
+                                        bigrams_dict_tfidf[phrase] = bigram[phrase] * (math.log(len(bigrams)/ float(1+bi_dict_df[phrase])))
+                        except KeyError:
+                                continue
+
                 bigrams_tfidf.append(bigrams_dict_tfidf)
-                                                
+
+        v_bigram_tfidf = DictVectorizer()
+        bigram_tfidf_data = v_bigram_tfidf.fit_transform(bigrams_tfidf)
+        bigram_corpus = v_bigram_tfidf.get_feature_names()
+        v_bigram_tf = DictVectorizer()
+        bigram_tf_data = v_bigram_tf.fit_transform(bigrams).toarray()
+                
+        N = len(bigrams)
+        avg = np.divide(bigram_tfidf_data.sum(axis=0), N)
+        sortedAvgIndices = np.argsort(avg)[::-1]
+        top_bigrams = [bigram_corpus[sortedAvgIndices[0,i]] for i in range(0, np.shape(sortedAvgIndices)[1])][0:termCount]
+
+        del bigrams_tfidf
+        del bigrams
+        
         trigrams = []
         tri_dict_corpus = {}
         tri_dict_df = {}
@@ -86,34 +112,37 @@ def get_bigrams_trigrams(text=[], urls=[], termCount=20, w2v=None, es=None):
                                 tri_dict_df[phrase] = tri_dict_df[phrase] + 1
                         else:
                                 tri_dict_df[phrase] = 1
+
+        if len(tri_dict_corpus) > MAX_PHRASES:
+                sorted_phrases = sorted(tri_dict_corpus.items(), key=operator.itemgetter(1), reverse=True)
+                tri_dict_corpus = {item[0]: tri_dict_corpus[item[0]] for item in sorted_phrases[0:MAX_PHRASES]}
+                tri_dict_df = {item[0]: tri_dict_df[item[0]] for item in sorted_phrases[0:MAX_PHRASES]}
+                
         trigrams_tfidf = []
         for trigram in trigrams:
                 trigrams_dict_tfidf = {}
-                for phrase in trigram.keys():
-                        if tri_dict_df[phrase] > 0: #(len(trigrams)/3):
-                                trigrams_dict_tfidf[phrase] = trigram[phrase] * (math.log(len(trigrams)/ float(1+tri_dict_df[phrase])))
+                for phrase in tri_dict_corpus.keys():
+                        try:
+                                if tri_dict_df[phrase] > 0: #(len(trigrams)/3):
+                                        trigrams_dict_tfidf[phrase] = trigram[phrase] * (math.log(len(trigrams)/ float(1+tri_dict_df[phrase])))
+                        except KeyError:
+                                continue
                 trigrams_tfidf.append(trigrams_dict_tfidf)
-                
-        v_bigram_tfidf = DictVectorizer()
-        bigram_tfidf_data = v_bigram_tfidf.fit_transform(bigrams_tfidf).toarray()
-        bigram_corpus = v_bigram_tfidf.get_feature_names()
-        v_bigram_tf = DictVectorizer()
-        bigram_tf_data = v_bigram_tf.fit_transform(bigrams).toarray()
-                
-        N = len(bigrams)
-        avg = np.divide(np.sum(bigram_tfidf_data, axis=0), N)
-        sortedAvgIndices = np.argsort(avg)[::-1]
-        top_bigrams = [bigram_corpus[i] for i in sortedAvgIndices[0:termCount]]
+
 
         v_trigram_tfidf = DictVectorizer()
-        trigram_tfidf_data = v_trigram_tfidf.fit_transform(trigrams_tfidf).toarray()
+        trigrams_set = Set()
+        for i in range(0, len(trigrams_tfidf)):
+                trigrams_set = trigrams_set.union(Set(trigrams_tfidf[i].keys()))
+
+        trigram_tfidf_data = v_trigram_tfidf.fit_transform(trigrams_tfidf)
         trigram_corpus = v_trigram_tfidf.get_feature_names()
         v_trigram_tf = DictVectorizer()
-        trigram_tf_data = v_trigram_tf.fit_transform(trigrams).toarray()
+        trigram_tf_data = v_trigram_tf.fit_transform(trigrams)
         
         N = len(trigrams)
-        avg = np.divide(np.sum(trigram_tfidf_data, axis=0), N)
+        avg = np.divide(trigram_tfidf_data.sum(axis=0), N)
         sortedAvgIndices = np.argsort(avg)[::-1]
-        top_trigrams = [trigram_corpus[i] for i in sortedAvgIndices[0:termCount]]
+        top_trigrams = [trigram_corpus[sortedAvgIndices[0,i]] for i in range(0, np.shape(sortedAvgIndices)[1])][0:termCount]
 
-        return bigram_tfidf_data, trigram_tfidf_data, bigram_tf_data, trigram_tf_data, bigram_corpus, trigram_corpus, bi_dict_corpus, tri_dict_corpus, top_bigrams, top_trigrams
+        return bigram_tfidf_data, trigram_tfidf_data, bigram_tf_data, trigram_tf_data, bigram_corpus, trigram_corpus, top_bigrams, top_trigrams
