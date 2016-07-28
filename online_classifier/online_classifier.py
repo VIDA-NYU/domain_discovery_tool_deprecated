23bd8e0448b0ee6af02181b80443a629d3e0ed49
Yamuna Krishnamurthy <yamuna@nyu.edu>
2016-07-28 18:31:18 -0400

    Fixed Errors

--- online_classifier/online_classifier.py
+++ online_classifier/online_classifier.py
@@ -3,6 +3,8 @@ from sklearn.calibration import CalibratedClassifierCV
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 
+import numpy as np
+
 class OnlineClassifier:
 
     def __init__(self):
@@ -10,32 +12,42 @@ class OnlineClassifier:
         self.count_vect = None
         self.tfidf_transformer = None
         
-    def vectorize(self, train, test):
+    def vectorize(self, train, test=[]):
         if self.count_vect is None:
             self.count_vect = CountVectorizer()
             X_train_counts = self.count_vect.fit_transform(train)
         else:
             X_train_counts = self.count_vect.transform(train)
-        X_test_counts = self.count_vect.transform(test)
+        if test:
+            X_test_counts = self.count_vect.transform(test)
+        else:
+            X_test_counts = None
 
         if self.tfidf_transformer is None:
             self.tfidf_transformer = TfidfTransformer()
             X_train = self.tfidf_transformer.fit_transform(X_train_counts)
         else:
             X_train = self.tfidf_transformer.fit_transform(X_train_counts)
-        X_test = self.tfidf_transformer.transform(X_test_counts)
+        if X_test_counts != None:
+            X_test = self.tfidf_transformer.transform(X_test_counts)
+        else:
+            X_test = None
 
         return [X_train, X_test]
 
     
     def fit(self, X, Y):
         clf = linear_model.SGDClassifier(n_iter=1)
-        clf.fit(X, Y)
+        try:
+            clf.fit(X, Y)
+        except ValueError as verr:
+            print("Value error: {0}".format(verr))
+            return None
         self.clf = clf
         return clf
 
     def partialFit(self, X, Y):
-        if clf is None:
+        if self.clf is None:
             self.fit(X, Y)
         else:
             self.clf.partial_fit(X,Y)
@@ -47,7 +59,7 @@ class OnlineClassifier:
         return sigmoid
 
     def calibrateScore(self, sigmoid, X, Y):
-        return sigmoid(X,Y)
+        return sigmoid.score(X,Y)
 
     def predictClass(self, X, Y, clf, sigmoid):
         for i in range(0,10):
