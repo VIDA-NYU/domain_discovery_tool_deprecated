cf7c0b21defa74d093d9a728b2c2d5a362e28579
Yamuna Krishnamurthy <yamuna@nyu.edu>
2016-08-09 10:25:03 -0400

    Fixed errors in computing accuracy

--- models/crawlermodel.py
+++ models/crawlermodel.py
@@ -86,6 +86,8 @@ class CrawlerModel:
     self._pos_tags = ['NN', 'NNS', 'NNP', 'NNPS', 'FW', 'JJ']
 
 
+    self.results_file = open("results.txt", "w")
+    
   # Returns a list of available crawlers in the format:
   # [
   #   {'id': crawlerId, 'name': crawlerName, 'creation': epochInSecondsOfFirstDownloadedURL},
@@ -873,9 +875,9 @@ class CrawlerModel:
       hits = self._getMoreLikePages(session)
     elif (session['pageRetrievalCriteria'] == 'Unsure tag'):
       hits = self._getUnsureLabelPages(session)
-    elif (session['pageRetrievalCriteria'] == 'Label pos'):
+    elif (session['pageRetrievalCriteria'] == 'Maybe relevant'):
       hits = self._getPosLabelPages(session)
-    elif (session['pageRetrievalCriteria'] == 'Label neg'):
+    elif (session['pageRetrievalCriteria'] == 'Maybe irrelevant'):
       hits = self._getNegLabelPages(session)
 
     return hits
@@ -1291,6 +1293,7 @@ class CrawlerModel:
 
     # Fit calibratrated classifier
 
+
     if train_data != None:
 
       trainedPosSamples = self._onlineClassifiers[session['domainId']]["trainedPosSamples"]
@@ -1325,70 +1328,80 @@ class CrawlerModel:
         
         sigmoid = self._onlineClassifiers[session['domainId']]["onlineClassifier"].calibrate(calibrate_data[train_indices], np.asarray(calibrate_labels)[train_indices])
         self._onlineClassifiers[session['domainId']]["sigmoid"] = sigmoid
-        accuracy = round(self._onlineClassifiers[session['domainId']]["onlineClassifier"].calibrateScore(sigmoid, calibrate_data[test_indices], np.asarray(calibrate_labels)[test_indices]), 2)
+        accuracy = round(self._onlineClassifiers[session['domainId']]["onlineClassifier"].calibrateScore(sigmoid, calibrate_data[test_indices], np.asarray(calibrate_labels)[test_indices]), 4) * 100
         self._onlineClassifiers[session['domainId']]["accuracy"] = str(accuracy)
 
-        print "\n\n\n Accuracy = ", accuracy, "\n\n\n"
-      
+        print "\n\n\n Accuracy = ", accuracy, "%\n\n\n"
+
       else:
-        print "\n\n\nNot data for calibration\n\n\n"
+        print "\n\n\nNot enough data for calibration\n\n\n"
 
       # ****************************************************************************************
 
-      # Label unlabelled data
+    # Label unlabelled data
 
-      if self._onlineClassifiers[session['domainId']].get("sigmoid") != None:
-        unlabelled_docs = field_missing(es_info["mapping"]["tag"], ["url", "text"], self._all,
-                                        es_info['activeCrawlerIndex'], 
-                                        es_info['docType'],
-                                        self._es)
-        
-        unlabeled_urls = [unlabelled_doc[es_info['mapping']['url']][0] for unlabelled_doc in unlabelled_docs]
-        unlabeled_ids = [unlabelled_doc["id"] for unlabelled_doc in unlabelled_docs]
-        unlabeled_text = [unlabelled_doc[es_info['mapping']['text']][0] for unlabelled_doc in unlabelled_docs]
-        
-        [unlabeled_data,_] =  self._onlineClassifiers[session['domainId']]["onlineClassifier"].vectorize(unlabeled_text)
-        [classp, calibp, cm] = self._onlineClassifiers[session['domainId']]["onlineClassifier"].predictClass(unlabeled_data,sigmoid)
-        
-        pos_calib_indices = np.nonzero(calibp)
-        neg_calib_indices = np.where(calibp == 0)
-        
-        pos_cm = [cm[pos_calib_indices][i][1] for i in range(0,np.shape(cm[pos_calib_indices])[0])]
-        neg_cm = [cm[neg_calib_indices][i][0] for i in range(0,np.shape(cm[neg_calib_indices])[0])]
+    unsure = 0
+    label_pos = 0
+    label_neg = 0
+    unlabeled_urls = []
 
-        pos_sorted_cm = pos_calib_indices[0][np.asarray(np.argsort(pos_cm)[::-1])]
-        neg_sorted_cm = neg_calib_indices[0][np.asarray(np.argsort(neg_cm)[::-1])]
+    sigmoid = self._onlineClassifiers[session['domainId']].get("sigmoid")
+    if sigmoid != None:
+      unlabelled_docs = field_missing(es_info["mapping"]["tag"], ["url", "text"], self._all,
+                                      es_info['activeCrawlerIndex'], 
+                                      es_info['docType'],
+                                      self._es)
+      
+      unlabeled_urls = [unlabelled_doc[es_info['mapping']['url']][0] for unlabelled_doc in unlabelled_docs]
+      unlabeled_ids = [unlabelled_doc["id"] for unlabelled_doc in unlabelled_docs]
+      unlabeled_text = [unlabelled_doc[es_info['mapping']['text']][0] for unlabelled_doc in unlabelled_docs]
+      
+      [unlabeled_data,_] =  self._onlineClassifiers[session['domainId']]["onlineClassifier"].vectorize(unlabeled_text)
+      [classp, calibp, cm] = self._onlineClassifiers[session['domainId']]["onlineClassifier"].predictClass(unlabeled_data,sigmoid)
+      
+      pos_calib_indices = np.nonzero(calibp)
+      neg_calib_indices = np.where(calibp == 0)
+      
+      pos_cm = [cm[pos_calib_indices][i][1] for i in range(0,np.shape(cm[pos_calib_indices])[0])]
+      neg_cm = [cm[neg_calib_indices][i][0] for i in range(0,np.shape(cm[neg_calib_indices])[0])]
+      
+      pos_sorted_cm = pos_calib_indices[0][np.asarray(np.argsort(pos_cm)[::-1])]
+      neg_sorted_cm = neg_calib_indices[0][np.asarray(np.argsort(neg_cm)[::-1])]
+      
+      entries = {}
+      for i in pos_sorted_cm:
+        entry = {}
+        if cm[i][1] < 60:
+          entry["unsure_tag"] = 1
+          entries[unlabeled_ids[i]] = entry
+          unsure = unsure + 1
+        else:
+          entry["label_pos"] = 1
+          entries[unlabeled_ids[i]] = entry
+          label_pos = label_pos + 1
 
-        entries = {}
-        for i in pos_sorted_cm:
-          entry = {}
-          if cm[i][1] < 60:
-            entry["unsure_tag"] = 1
-            entries[unlabeled_ids[i]] = entry
-          else:
-            entry["label_pos"] = 1
-            entries[unlabeled_ids[i]] = entry
-
-        for i in neg_sorted_cm:
-          entry = {}
-          if cm[i][0] < 60:
-            entry["unsure_tag"] = 1
-            entries[unlabeled_ids[i]] = entry
-          else:
-            entry["label_neg"] = 1
-            entries[unlabeled_ids[i]] = entry
+      for i in neg_sorted_cm:
+        entry = {}
+        if cm[i][0] < 60:
+          entry["unsure_tag"] = 1
+          entries[unlabeled_ids[i]] = entry
+          unsure = unsure + 1
+        else:
+          entry["label_neg"] = 1
+          entries[unlabeled_ids[i]] = entry
+          label_neg = label_neg + 1
             
-        if entries:
-          update_try = 0
-          while (update_try < 10):
-            try:
-              update_document(entries, es_info['activeCrawlerIndex'], es_info['docType'], self._es)
-              break
-            except:
-              update_try = update_try + 1
-              
-        pos_indices = np.nonzero(classp)
-        neg_indices = np.where(classp == 0)
+      if entries:
+        update_try = 0
+        while (update_try < 10):
+          try:
+            update_document(entries, es_info['activeCrawlerIndex'], es_info['docType'], self._es)
+            break
+          except:
+            update_try = update_try + 1
+            
+      pos_indices = np.nonzero(classp)
+      neg_indices = np.where(classp == 0)
 
     accuracy = '0'
     if self._onlineClassifiers.get(session['domainId']) != None:
@@ -1396,6 +1409,8 @@ class CrawlerModel:
       if accuracy == None:
         accuracy = '0'
 
+    self.results_file.write(str(len(pos_text)) +","+ str(len(neg_text)) +","+ accuracy +","+ str(unsure) +","+ str(label_pos) +","+ str(label_neg) +","+ str(len(unlabeled_urls))+"\n")
+    
     return accuracy
       
     
