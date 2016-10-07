9ecac83e4c4ff06feafb070511ff8a4721831abd
Yamuna Krishnamurthy <yamuna@nyu.edu>
2016-10-07 10:37:48 -0400

    Return term frequency matrix

--- online_classifier/tfidf_vector.py
+++ online_classifier/tfidf_vector.py
@@ -5,17 +5,17 @@ from tf_vector import tf_vectorizer
 
 class tfidf_vectorizer(tf_vectorizer):
     
-    def __init__(self, convert_to_ascii=False, max_features= 10000):
+    def __init__(self, convert_to_ascii=False, max_features= 10000, ngram_range=(1,1)):
         self.tfidf_transformer = None
-        tf_vectorizer.__init__(self, convert_to_ascii, max_features)
+        tf_vectorizer.__init__(self, convert_to_ascii, max_features, ngram_range)
         
     def tfidf(self, data):
-        [X_counts, _] = self.vectorize(data)
+        [X_counts, features] = self.vectorize(data)
         if self.tfidf_transformer is None:
             self.tfidf_transformer = TfidfTransformer()
             X = self.tfidf_transformer.fit_transform(X_counts)
         else:
             X = self.tfidf_transformer.transform(X_counts)
 
-        return [X, self.count_vect.get_feature_names()]
+        return [X, X_counts, features]
 
