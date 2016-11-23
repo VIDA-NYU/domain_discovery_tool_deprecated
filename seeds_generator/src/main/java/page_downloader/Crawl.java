e8ea05aa64212cf8cc414770c1d428239a46e058
Yamuna Krishnamurthy <yamuna@nyu.edu>
2016-11-23 12:24:23 -0500

    Fixed error in forward crawling

--- seeds_generator/src/main/java/page_downloader/Crawl.java
+++ seeds_generator/src/main/java/page_downloader/Crawl.java
@@ -61,24 +61,18 @@ public class Crawl {
 
     public void addForwardCrawlTask(ArrayList<String> urls, String top){
 	try{
-	    MissingFilterBuilder filter=FilterBuilders.missingFilter("crawled_forward");
-	    QueryBuilder qb = QueryBuilders.filteredQuery(QueryBuilders.matchAllQuery(),filter);
-	    SearchResponse searchResponse = client.prepareSearch(this.es_index)
-		.setTypes(this.es_doc_type)
-		.setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
-		.setFetchSource(new String[]{"url"}, null)
-		.setQuery(qb)
-		.setSize(this.MAXSIZE)
-		.setFrom(0).setExplain(true)
-		.execute()
-		.actionGet();
-	    
-	    ArrayList<String> not_crawled = new ArrayList<String>();
-	    for (SearchHit hit : searchResponse.getHits()) {
-		Map map = hit.getSource();
-		String url = (String)map.get("url");
-		if(urls.contains(url)){
-		    not_crawled.add(url);
+	    for (String f_url : urls) {
+		SearchResponse searchResponse = client.prepareSearch(this.es_index)
+		    .setTypes(this.es_doc_type)
+		    .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+		    .setFetchSource(new String[]{"url"}, null)
+		    .setQuery(QueryBuilders.termQuery("url", f_url))
+		    .setSize(this.MAXSIZE)
+		    .setFrom(0).setExplain(true)
+		    .execute()
+		    .actionGet();
+		
+		for (SearchHit hit : searchResponse.getHits()) {
 		    UpdateRequest updateRequest = new UpdateRequest(this.es_index, this.es_doc_type, hit.getId())
 			.doc(XContentFactory.jsonBuilder()
 			     .startObject()
@@ -88,7 +82,7 @@ public class Crawl {
 		}
 	    }
 
-	    crawlForwardService.execute(new CrawlerInterface(not_crawled, null, "forward", top, this.es_index, this.es_doc_type, this.es_host, this.client));
+	    crawlForwardService.execute(new CrawlerInterface(urls, null, "forward", top, this.es_index, this.es_doc_type, this.es_host, this.client));
 	} catch (IOException e1) {
 	    // TODO Auto-generated catch block
 	    e1.printStackTrace();
